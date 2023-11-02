9ade045fb1b043922e50156f510dee0f478727f5
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-11-03 00:18:57 +0700

    Update Payload Verify

--- pet_store_backend.api/Common/Mapping/AuthenticationMappingConfig.cs
+++ pet_store_backend.api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -14,6 +14,7 @@ public class AuthenticationMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        config.NewConfig<VerifyRequest, VerifyCommand>();
         config.NewConfig<ForgotPasswordRequest, ForgotPasswordCommand>();
         config.NewConfig<RegisterRequest, RegisterCommand>();
         config.NewConfig<PasswordResetRequest, ResetPasswordCommand>();
