35a8a2efd0dee1321bff79f1045a83b899d86562
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-12-18 13:42:37 +0700

    Update Add Cart

--- src/backend/pet_store_backend.application/Order/Commands/OrderProductCommandHandler.cs
+++ src/backend/pet_store_backend.application/Order/Commands/OrderProductCommandHandler.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using pet_store_backend.application.Common;
 using pet_store_backend.application.Common.Interfaces.Persistence;
 using pet_store_backend.domain.Common.Errors;
+using pet_store_backend.domain.Entities.Orders;
 
 namespace pet_store_backend.application.Order.Commands;
 
@@ -44,21 +45,28 @@ public class OrderProductCommandHandler : IRequestHandler<OrderProductCommand, E
 
     public async Task<ErrorOr<MessageResult>> Handle(OrderProductCommand request, CancellationToken cancellationToken)
     {
+        var customerId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (customerId == null)
+        {
+            return Errors.User.UserNotSignIn;
+        }
+
         if (!(await _collectionRepository.CheckProductIsValid(Guid.Parse(request.ProductId))))
         {
             return Errors.Product.NullProduct;
         }
-        // if ((await _orderRepository.CheckProductOrderIsExist(Guid.Parse(request.ProductId), Guid.Parse(customerId)) is OrderProduct orderProduct))
-        // {
-        //     // int updateQuantity = request.Quantity + orderProduct.Quantity;
-        //     orderProduct.UpdateQuantityOrderProduct(request.Quantity);
-        //     await _orderRepository.UpdateOrderProduct(orderProduct);
-        // }
-        // else
-        // {
-        if (!await _orderRepository.AddProductOrder(Guid.Parse(request.ProductId), 1))
+        if ((await _orderRepository.CheckProductOrderIsExist(Guid.Parse(request.ProductId), Guid.Parse(customerId)) is OrderProduct orderProduct))
         {
-            return Errors.Order.OrderProductAddProblem;
+            int updateQuantity = 1 + orderProduct.Quantity;
+            orderProduct.UpdateQuantityOrderProduct(updateQuantity);
+            await _orderRepository.UpdateOrderProduct(orderProduct);
+        }
+        else
+        {
+            if (!await _orderRepository.AddProductOrder(Guid.Parse(request.ProductId), 1))
+            {
+                return Errors.Order.OrderProductAddProblem;
+            }
         }
 
         return new MessageResult("Add to Cart success !");
